d8bc088251c3380bf57342083fe249bac4c81d8a
rudenkoserhii <rudenko.sergey.v@gmail.com>
2024-03-15 17:05:30 +0200

    feat: added app

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,48 @@
-import React from 'react'
-import GlobalStyles from 'styles/GlobalStyle'
-function App() {
-  return (
-    <div className="App">
-      <GlobalStyles />
-      <header className="App-header"></header>
-      <p>Hello World</p>
-    </div>
+import React, { useEffect, lazy } from 'react'
+import { useDispatch } from 'react-redux'
+import { Route, Routes } from 'react-router-dom'
+import { LayOut } from 'components/LayOut/LayOut'
+import { refreshUser } from 'store/auth/operations'
+import { useAuth } from 'hooks'
+import { Loading } from 'components/common/Loading/Loading'
+import Notiflix from 'notiflix'
+
+const Login = lazy(() => import('pages/LoginPage/LoginPage'))
+const ForgotPassword = lazy(() => import('pages/ForgotPasswordPage/ForgotPasswordPage'))
+const ResetPassword = lazy(() => import('pages/ResetPasswordPage/ResetPasswordPage'))
+
+const App = () => {
+  const dispatch = useDispatch()
+  const { isRefreshing, token } = useAuth()
+
+  useEffect(() => {
+    ;(() => {
+      if (token) {
+        try {
+          const response = dispatch(refreshUser())
+          if (response.meta.requestStatus === 'rejected') {
+            Notiflix.Notify.failure(`Something went wrong - ${response.payload}!`)
+            return
+          }
+        } catch (error) {
+          Notiflix.Notify.failure(`Something went wrong - ${error.message}`)
+        }
+      }
+    })()
+  }, [dispatch, token])
+
+  return isRefreshing ? (
+    <Loading isVisible={true} />
+  ) : (
+    <Routes>
+      <Route path="/" redirectTo="/login" element={<LayOut />}>
+        <Route index redirectTo="/login" element={<Login />} />
+        <Route path="*" redirectTo="/login" element={<Login />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/reset-password" element={<ResetPassword />} />
+      </Route>
+    </Routes>
   )
 }
 
--- src/index.js
+++ src/index.js
@@ -5,14 +5,24 @@ import reportWebVitals from 'reportWebVitals'
 import { ThemeProvider } from 'styled-components'
 import theme from 'styles/theme'
 import GlobalStyles from 'styles/GlobalStyle'
+import { BrowserRouter } from 'react-router-dom'
+import { PersistGate } from 'redux-persist/integration/react'
+import { Provider } from 'react-redux'
+import { store, persistor } from 'store/store'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <GlobalStyles />
-      <App />
-    </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <PersistGate loading={null} persistor={persistor}>
+          <BrowserRouter basename="/">
+            <GlobalStyles />
+            <App />
+          </BrowserRouter>
+        </PersistGate>
+      </ThemeProvider>
+    </Provider>
   </React.StrictMode>,
 )
 
